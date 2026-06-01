c813f5421bdff5341512f03d039168f3aab1bf89
unknown <josutic.sutic@gmail.com>
2026-06-01 14:40:27 +0200

    feat(3): check for the log

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,10 @@
-import 'dotenv/config'
-import { z } from 'zod'
+import "dotenv/config";
+import { z } from "zod";
 
 const envSchema = z.object({
-  DATABASE_URL: z.url(),
-})
+  DATABASE_URL: z.string(),
+});
 
-export const env = envSchema.parse(process.env)
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
+
+export const env = envSchema.parse(process.env);
