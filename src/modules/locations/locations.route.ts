adcca9d546fcb1acf7697651e65a5900ed32c4b2
unknown <josutic.sutic@gmail.com>
2026-06-01 21:59:13 +0200

    feat(4): update location service and route

--- src/modules/locations/locations.route.ts
+++ src/modules/locations/locations.route.ts
@@ -1,23 +1,21 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
-
-import {
-  getLocationsRoute,
-  createLocationRoute,
-} from "./locations.contract";
-
+import { getLocationsRoute, createLocationRoute } from "./locations.contract";
 import { locationsService } from "./locations.service";
+import { App } from "@/types";
 
-const app = new OpenAPIHono();
+const app = new OpenAPIHono<App>();
 
 app.openapi(getLocationsRoute, async (c) => {
-  const locations = await locationsService.getAll();
+  const db = c.get("db");
+  const locations = await locationsService.getAll(db);
   return c.json(locations, 200);
 });
 
 app.openapi(createLocationRoute, async (c) => {
+  const db = c.get("db");
   const body = c.req.valid("json");
 
-  const location = await locationsService.create(body);
+  const location = await locationsService.create(db, body);
 
   return c.json(location, 201);
 });
